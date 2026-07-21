fe19af7cbd9e0361798fc8e281227d65232c3171
yunum811-bit <yunum811@gmail.com>
2026-07-21 16:38:39 +0700

    ีupdate

---
+++ gitpush.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d "D:\Web\serveyMKT"
+"C:\Program Files\Git\bin\git.exe" push https://yunum811-bit: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@github.com/yunum811-bit/serveyMKT.git main > C:\Temp\gitout.txt 2>&1
+type C:\Temp\gitout.txt
