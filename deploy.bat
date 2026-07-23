20deb148b634d293deabefea7b2a8944767944c0
yunum811-bit <yunum811@gmail.com>
2026-07-23 09:34:24 +0700

    ีup

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,10 @@
 @echo off
 echo === Git Push to GitHub ===
 cd /d "D:\Web\serveyMKT"
-"C:\Program Files\Git\bin\git.exe" remote set-url origin https://yunum811-bit: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@github.com/yunum811-bit/serveyMKT.git
-"C:\Program Files\Git\bin\git.exe" add -A
+"C:\Program Files\Git\bin\git.exe" config credential.helper store
+echo https://yunum811-bit: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@github.com > "%USERPROFILE%\.git-credentials"
+"C:\Program Files\Git\bin\git.exe" remote set-url origin https://github.com/yunum811-bit/serveyMKT.git
+"C:\Program Files\Git\bin\git.exe" add script.js index.html admin.html
 "C:\Program Files\Git\bin\git.exe" commit -m "fix: resolve all merge conflicts" --allow-empty
 "C:\Program Files\Git\bin\git.exe" push origin main
 echo === Done ===
